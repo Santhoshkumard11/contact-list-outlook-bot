b4bceb06c80ac841324dfdae444a80c9d260a662
SANTHOSH KUMAR D <santhoshkdhana@gmail.com>
2021-08-09 17:53:30 +0530

    Update contact_list_adder.py
    added login to iterate and type the contact names and add it

--- contact_list_adder.py
+++ contact_list_adder.py
@@ -14,19 +14,34 @@ def load_contact_list():
 
 
 
-
 def click_space(x,y):
+    """ Move the cursor around with just the coordinates """
     moveTo(x,y)
     sleep(0.4)
     click()
-    
-    
-    
+
+
+
 # loading the emails to a contact_list from text file
 print("Loading contact list")
 load_contact_list()
-print("adding contact list")
 
-
+print("clicking on the add contact field")
 click_space(1019,532)
 start_time = time.time()
+
+# iterating through the entire contact list
+for index, contact in enumerate(contact_list):
+    
+    sleep(0.1)
+    
+    # click on the add contact filed
+    
+    # paste the email address
+    print(index, contact)
+
+    typewrite(contact)
+
+    sleep(0.31)
+
+    press('enter')
